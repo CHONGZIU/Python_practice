48de3c8f014e491109a5f467ee045aff89634902
CHONGZIU <ziuchong@gmail.com>
2023-09-22 10:22:47 +0900

    09210921

--- practicePY/0921_logic06.py
+++ practicePY/0921_logic06.py
@@ -1,14 +1,15 @@
 list = [1, 2, 3, 4]
 
-def line_up(arr, a): # a는 현재 순열의 시작위치
-    if a == len(arr): # a가 arr의 길이와 같아질 때, 원소의 모든 순열을 생성한 경우
-        print(arr) # 함수를 출력하고 종료
+def line_up(arr, a): # aは現在順列の始まる位置
+    if a == len(arr): # aがarrの長さと同じようになった時、配列中の数字、全部順列を生成した場合
+        print(arr) # 関数を出力して終わり
         return
     
     for i in range(a, len(arr)):
-        arr[a], arr[i] = arr[i], arr[a]  # 순서를 바꾸어 다음 원소를 선택      
-        line_up(arr, a + 1)  # 재귀 호출
-        arr[a], arr[i] = arr[i], arr[a] 
+        arr[a], arr[i] = arr[i], arr[a] # 手順を変わって次の数字を選ぶ
+        line_up(arr, a + 1) # 再帰呼び出す
+        arr[a], arr[i] = arr[i], arr[a]  
+
 
 line_up(list, 0)
 

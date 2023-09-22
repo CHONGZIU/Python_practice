fe81e8e815525f50f5c22ce5039118e9c5796acf
CHONGZIU <ziuchong@gmail.com>
2023-09-22 11:51:24 +0900

    0922

--- practicePY/0922_logic09.py
+++ practicePY/0922_logic09.py
@@ -9,3 +9,7 @@ while x > 0: # 分けることがないまでに分けるための反復門
     
 for i in range(len(arr)-1, -1, -1): # 込んだ残りの配列を逆に出力するための反復門
     print(arr[i], end = "")
+print()
+
+for j in reversed(range(len(arr))): # 込んだ残りの配列を逆に出力するための他の反復門
+    print(arr[j], end = "")

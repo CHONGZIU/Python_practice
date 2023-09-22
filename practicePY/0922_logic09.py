18beebe7910205665c58576e34370fbbdfe50030
CHONGZIU <ziuchong@gmail.com>
2023-09-22 11:43:35 +0900

    0922

---
+++ practicePY/0922_logic09.py
@@ -0,0 +1,11 @@
+x = input()
+x = int(x)
+arr = []
+
+while x > 0: # 分けることがないまでに分けるための反復門
+    a = x % 2 # 残り
+    arr.append(a) # 残りを配列に込む
+    x = x // 2 # 分け前を変更    
+    
+for i in range(len(arr)-1, -1, -1): # 込んだ残りの配列を逆に出力するための反復門
+    print(arr[i], end = "")

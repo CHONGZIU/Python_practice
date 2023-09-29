db31bf5f195cea07932859e3bbf29bf82c6307c1
CHONGZIU <ziuchong@gmail.com>
2023-09-29 13:47:48 +0900

    python

---
+++ practicePY/0929/0929_python_practice.py
@@ -0,0 +1,24 @@
+name = "ziu"
+birthplace = "korea"
+
+print("My name is {}. I'm from {}.".format(name, birthplace))
+print(f"My name is {name}. I'm from {birthplace}.") # 이걸 쓰는 걸 더 추천한다
+'''ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ'''
+print("i'm a mess in this dress. but, we still the best dress.")
+print("i'm a mess in this dress. \nbut, we still the best dress.") # 줄 바꿈
+'''ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ'''
+print("decimal = %d, hexadecimal = %x, float = %f" % (16, 16, 16)) # 각 진수별로 표기하는 기호 %d(10진수) %x(16진수) %f(소수점있는 10진수)
+'''ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ'''
+letter = "AbCdEfGh"
+print(letter.upper())
+print(letter.lower())
+'''ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ'''
+msg = "apple banana orange"
+print(msg.split())
+msg = "Jade, David, Kevin, Lucy"
+print(msg.split(","))
+'''ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ'''
+message = "how ever"
+msg = message.split()
+print(msg)
+print("+".join(msg))
